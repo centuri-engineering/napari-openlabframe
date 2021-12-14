86894a1b887fbdbc25e8b1bb2a690edec4aaffa4
Guillaume Gay <guillaume@damcb.com>
2021-12-14 17:26:11 +0100

    debugged motor ctrl

--- src/napari_openlabframe/_dock_widget.py
+++ src/napari_openlabframe/_dock_widget.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
 
 
-class WebcamControler(widgets.Container):
+class OlfControler(widgets.Container):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -22,11 +22,12 @@ class WebcamControler(widgets.Container):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
-        #  self.extend([get_image, set_resolution])
+        self.extend([get_image, set_resolution])
         self.extend([set_position, Stepper()])
+        # self.extend([set_position])
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
-    return WebcamControler
+    return OlfControler
